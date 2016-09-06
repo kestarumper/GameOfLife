a79dc6bc0b9d83e6ab69486db720b2ab46cc676d
Adrian <kestarumper8@gmail.com>
2016-09-06 22:10:21 +0200

    added cycles

--- Cell.js
+++ Cell.js
@@ -1,10 +1,61 @@
 class Cell {
-    constructor(id) {
+    constructor(x,y) {
         this.obj = document.createElement("td");
+        this.pos_x = x;
+        this.pos_y = y;
+        this.checked = false;
+        this.aliveNeighbours = 0;
 
-        this.getDOMObj().id = `cell_${id}`;
+        this.getDOMObj().onclick = () => {
+            this.getDOMObj().style.backgroundColor = "green";
+            this.makeAlive();
+        }
+
         this.getDOMObj().className = "cell";
+        this.getDOMObj().setAttribute("pos_x",this.pos_x);
+        this.getDOMObj().setAttribute("pos_y",this.pos_y);
         this.getDOMObj().setAttribute("dead","");
+    }
+
+    isAlive(){
+        return this.getDOMObj().hasAttribute("alive");
+    }
+
+    isChecked(){
+        return this.checked;
+    }
+
+    setChecked(state){
+        this.checked = state;
+    }
+
+    getPos(){
+        return {
+            x: this.pos_x,
+            y: this.pos_y
+        };
+    }
+
+    checkNeighbours(world){
+        if(!this.isChecked()){
+            this.setChecked(true);
+
+            let aliveNeighbours = 0;
+
+            for(let i = -1; i < 2; i++){
+                for(let j = -1; j < 2; j++){
+                    let neighbour = world.getCell(this.getPos().x+j, this.getPos().y+i);
+                    if(neighbour != undefined){
+                        if(neighbour != this){
+                            if(neighbour.isAlive()){
+                                aliveNeighbours++;
+                            }
+                        }
+                    }
+                }
+            }
+            this.aliveNeighbours = aliveNeighbours;
+        }
     }
 
     makeAlive(){
--- World.js
+++ World.js
@@ -15,11 +15,15 @@ class World {
             this.worldGrid.appendChild(row);
 
             for (let j = 0; j < this.width; j++) {
-                let cell = new Cell(j);
+                let cell = new Cell(j,i);
                 this.cellTable.push(cell);
                 row.appendChild(cell.getDOMObj());
             }
         }
+    }
+
+    getCell(x,y){
+        return this.cellTable[y*this.width+x];
     }
 
     getCells(){
@@ -31,6 +35,31 @@ class World {
             console.error(`${figure.constructor.name} is not a valid object.`);
         }else {
             figure.render();
-        }        
+        }
+    }
+
+    tick(){
+        for(let cell of this.getCells()){
+            if(cell.isAlive()){
+                for(let i = -1; i < 2; i++){
+                    for(let j = -1; j < 2; j++){
+                        let neighbour = this.getCell(cell.getPos().x+j, cell.getPos().y+i);
+                        if(neighbour != undefined){
+                            neighbour.checkNeighbours(this);
+                        }
+                    }
+                }
+            }
+        }
+        for(let cell of this.getCells()){
+            if(!cell.isAlive() && cell.aliveNeighbours == 3){
+                cell.makeAlive();
+            }
+            if(cell.isAlive() && (cell.aliveNeighbours != 2 && cell.aliveNeighbours != 3)){
+                cell.makeDead();
+            }
+            cell.setChecked(false);
+            cell.aliveNeighbours = 0;
+        }
     }
 }
